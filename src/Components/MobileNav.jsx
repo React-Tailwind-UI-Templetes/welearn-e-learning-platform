35c2301e0ca7a92f563028e852b410748cf06540
Asfak Ahmed <asfakahmed680@gmail.com>
2024-03-04 00:41:30 +0600

    website complete

--- src/Components/MobileNav.jsx
+++ src/Components/MobileNav.jsx
@@ -30,7 +30,7 @@ const MobileNav = () => {
       <nav
         className={`${
           scrolling && "backdrop-blur-lg shadow-md"
-        } sticky top-0 left-0 w-full max-w-[1600px] mx-auto px-6 py-4 flex items-center justify-between gap-5 z-50`}
+        } sticky top-0 left-0 w-full max-w-[1600px] mx-auto px-6 py-4 flex items-center justify-between gap-5 z-50 md:hidden`}
       >
         <img src="/logo.png" alt="logo" className="w-[110px]" />
 
